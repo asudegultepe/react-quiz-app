f9a9309df1dc73862d545ae2d7a363401b5b596b
asudegultepe <gultepeasude@gmail.com>
2022-05-16 13:24:48 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import QuestionCard from './components/QuestionCard';
 //Types
 import { QuestionState, Difficulty } from './API';
 
-type AnswerObject = {
+export type AnswerObject = {
   question: string;
   answer: string;
   correct: boolean;
-  coorectAnswer: string;
+  correctAnswer: string;
 }
 
 const TOTAL_QUESTIONS = 10;
@@ -18,7 +18,7 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
@@ -40,36 +40,60 @@ const App = () => {
   };
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-
-  }
+    if (!gameOver) {
+      //User answer
+      const answer = e.currentTarget.value;
+      //Check answer against the correct answer
+      const correct = questions[number].correct_answer === answer;
+      //Add score if answer is correct
+      if (correct) setScore(prev => prev + 1);
+      //Save answer in the array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  };
 
   const nextQuestion = () => {
+    //Move on to the next question !last_question
+    const nextQuestion = number + 1;
 
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
   }
 
   return (
     <div className="App">
       <h1>React Quiz</h1>
       {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
-        <button className="start" onClick={startTrivia} >
+        <button className='start' onClick={startTrivia}>
           Start
         </button>
       ) : null}
-      {!gameOver ? <p className="score">Score</p> : null}
-      {loading && <p>Loading Questions...</p>}
+      {!gameOver ? <p className='score'>Score: {score}</p> : null}
+      {loading ? <p>Loading Questions...</p> : null}
       {!loading && !gameOver && (
-      <QuestionCard 
-        questionNr={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      />
-      )};
-      <button className='next' onClick={nextQuestion} >
-        Next Question
-      </button>
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+        <button className='next' onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 }
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,37 +1,39 @@
 import React from "react";
 
-type Props ={
+import { AnswerObject } from "../App";
+
+type Props = {
     question: string;
     answers: string[];
-    callback: any;
-    userAnswer: any;
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    userAnswer: AnswerObject | undefined;
     questionNr: number;
     totalQuestions: number;
 }
 
-const QuestionCard: React.FC<Props> = ({ 
+const QuestionCard: React.FC<Props> = ({
     question,
     answers,
-    callback, 
+    callback,
     userAnswer,
     questionNr,
     totalQuestions,
-    }) => (
-       <div>
-           <p className="number">
-               Question: {questionNr} / {totalQuestions}
-           </p>
-           <p dangerouslySetInnerHTML={{ __html: question }} />
-           <div>
-               {answers.map(answer => (
-                   <div>
-                       <button disabled={userAnswer} onClick={callback}>
-                           <span dangerouslySetInnerHTML={{ __html: answer }} />
-                       </button>
-                   </div>
-                ))}
-           </div>
-       </div>
+}) => (
+    <div>
+        <p className="number">
+            Question: {questionNr} / {totalQuestions}
+        </p>
+        <p dangerouslySetInnerHTML={{ __html: question }} />
+        <div>
+            {answers?.map(answer => (
+                <div key={answer}>
+                    <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+                        <span dangerouslySetInnerHTML={{ __html: answer }} />
+                    </button>
+                </div>
+            ))}
+        </div>
+    </div>
 );
 
 export default QuestionCard;
